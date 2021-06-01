52b5c4f82ed8882bc5bd95540e920c7694a32e55
James-Coombs-72 <74391191+James-Coombs-72@users.noreply.github.com>
2021-06-01 09:38:24 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+async function runAPIQuery(){
+    const url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+
+    const initObject = {
+        method: 'GET'
+    }
+
+    const resp = await fetch(url, initObject)
+        .then(response => response.json())
+    
+    $('#body').append(`
+        <p class="NiceText">${resp.time.updated}</p>
+        <p class="NiceText">BitCoin Price Index USD: ${resp.bpi.USD.rate}</p>
+    `)
+}
